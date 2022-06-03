1dde3507adb488affd02a9c6bc828a96d8782b05
Bradley Lostak <bradleycl98@gmail.com>
2022-06-03 16:56:48 -0700

    add create whitelisted pool msg to balancer and reorganize proto file structure

--- x/gamm/pool-models/balancer/msgs.go
+++ x/gamm/pool-models/balancer/msgs.go
@@ -9,12 +9,69 @@ import (
 
 const (
 	TypeMsgCreateBalancerPool = "create_balancer_pool"
+	TypeMsgCreateWhitelistedBalancerPool = "create_whitelisted_balancer_pool"
 )
 
 var (
+	// TODO figure out what this actually means
+	// Is this the glue for PoolI?
 	_ sdk.Msg             = &MsgCreateBalancerPool{}
 	_ types.CreatePoolMsg = &MsgCreateBalancerPool{}
+	
+	_ sdk.Msg 			  			 = &TypeMsgCreateWhitelistedBalancerPool{}
+	_ types.CreateWhitelistedPoolMsg = &TypeMsgCreateWhitelistedBalancerPool{}
 )
+
+func NewMsgCreateWhitelistedBalancerPool(
+	sender sdk.AccAddress,
+	poolParams PoolParams,
+	poolAssets []PoolAsset,
+	futurePoolGovernor string,
+	exclusive bool,
+) MsgCreateWhitelistedBalancerPool {
+	return MsgCreateWhitelistedBalancerPool{
+		Sender:				sender.String(),
+		PoolParams:			&poolParams,
+		PoolAssets:			poolAssets,
+		FuturePoolGovernor:	futurePoolGovernor,
+	}
+}
+
+func (msg MsgCreateWhitelistedBalancerPool) Route() string { return types.RouterKey }
+func (msg MsgCreateWhitelistedBalancerPool) Type() string { return TypeMsgCreateWhitelistedBalancerPool }
+func (msg MsgCreateWhitelistedBalancerPool) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid sender address (%s)", err)
+	}
+
+	err = ValidateUserSpecifiedPoolAssets(msg.PoolAssets)
+	if err != nil {
+		return err
+	}
+
+	err = msg.PoolParams.Validate(msg.PoolAssets)
+	if err != nil {
+		return err
+	}
+
+	// validation for future owner
+	if err = types.ValidateFutureGovernor(msg.FuturePoolGovernor); err != nil {
+		return err
+	}
+}
+
+func (msg MsgCreateWhitelistedBalancerPool) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(&msg))
+}
+
+func (msg MsgCreateWhitelistedBalancerPool) GetSigners() []sdk.AccAddress {
+	sender, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{sender}
+}
 
 func NewMsgCreateBalancerPool(
 	sender sdk.AccAddress,
@@ -97,4 +154,41 @@ func (msg MsgCreateBalancerPool) InitialLiquidity() sdk.Coins {
 func (msg MsgCreateBalancerPool) CreatePool(ctx sdk.Context, poolID uint64) (types.PoolI, error) {
 	poolI, err := NewBalancerPool(poolID, *msg.PoolParams, msg.PoolAssets, msg.FuturePoolGovernor, ctx.BlockTime())
 	return &poolI, err
+}
+
+/// Implement the CreateWhitelistedPoolMsg interface
+
+func (msg MsgCreateWhitelistedBalancerPool) PoolCreator() sdk.AccAddress {
+	sender, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		panic(err)
+	}
+	return sender
+}
+
+func (msg MsgCreateWhitelistedBalancerPool) Validate(ctx sdk.Context) error {
+	return msg.ValidateBasic()
+}
+
+func (msg MsgCreateWhitelistedBalancerPool) InitialLiquidity() sdk.Coins {
+	var coins sdk.Coins
+	for _, asset := range msg.PoolAssets {
+		coins = append(coins, asset.Token)
+	}
+	if coins == nil {
+		panic("Shouldn't happen")
+	}
+	coins = coins.Sort()
+	return coins
+}
+
+func (msg MsgCreateWhitelistedBalancerPool) CreatePool(ctx sdk.Context, poolID uint64) (types.PoolI, error) {
+	poolI, err := NewBalancerPool(poolID, *msg.PoolParams, msg.PoolAssets, msg.FuturePoolGovernor, ctx.BlockTime())
+	return &poolI, err
+}
+
+func (msg MsgCreateWhitelistedBalancerPool) CreateWhitelist(ctx sdk.Context, poolID uint64, exclusive bool) (types.WhitelistI, error) {
+	// create and return basic whitelist
+	whitelistI, err := NewBasicWhitelist(poolID, exclusive, msg.Sender) 
+	return &whitelistI, err 
 }
--- x/gamm/types/msg_create_whitelisted_pool.go
+++ x/gamm/types/msg_create_whitelisted_pool.go
@@ -4,10 +4,10 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 
 // CreatePoolMsg defines an interface that every CreatePool transaction should implement.
 // The gamm logic will use this to create a pool and associated governor.
-type CreatePoolWithGovMsg interface {
+type CreateWhitelistedPoolMsg interface {
 	CreatePoolMsg
 	
 	// CreateGovernor makes the msg signer the governor for the pool 
 	// with an empty whitelist and exclusivity initially true. 
-	CreateGovernor(ctx sdk.Context, poolID uint64, sender string) (governor, error)
+	CreateWhitelistedPool(ctx sdk.Context, poolID uint64, sender string) (WhitelistI, error)
 }
